aaf57da62999482c5da11d4f86c864c77508d6b0
ssadasd <eelesort@gmail.com>
2026-05-11 21:03:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const WEB_APP_URL = "TU_URL_DE_APPS_SCRIPT";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbweUshmxJOmi_axjM1JMmzAtvdqIZAFrQftSU1BSEAac9DwWaRJTFI3yp7WG8amouB1/exec";
 
 const form = document.getElementById("raffleForm");
 const submitButton = document.getElementById("submitButton");
